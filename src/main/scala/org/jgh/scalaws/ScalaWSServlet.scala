40f892be3f061e6c1a6e6a9bb77add3c0fea54aa
Jeremy Hare <jeremy.hare@gmail.com>
2009-09-25 12:05:06 +1000

    wrapped lines for readability renamed sql script

--- src/main/scala/org/jgh/scalaws/ScalaWSServlet.scala
+++ src/main/scala/org/jgh/scalaws/ScalaWSServlet.scala
@@ -9,7 +9,8 @@ trait DBAdapter {
   def datasource:javax.sql.DataSource  //A dependency. Declared as abstract, needs to be supplied by classes that use this trait
   //consise. this line declares a class with two immutable fields (use var for variables) and a constructor
   class Param(val sqlType:Int,val value:Any)
-  //object keyword declares a singleton. Singleton pattern is part of the language. lazily instanciated. No statics in scala.
+  //object keyword declares a singleton. Singleton pattern is part of the language.
+  //Lazily instanciated. No statics in scala.
   object Param {
     //Objects can be functions by implementing an apply method.
     def apply(s:String) = new  Param(Types.VARCHAR, s)
@@ -17,7 +18,8 @@ trait DBAdapter {
     def apply(i:BigInt) = new  Param(Types.BIGINT, i)
   }
 
-  //Functions are objects and can be passed as parameters . c is a function that takes a Conn as a parameter and returns a sequence of Nodes.
+  //Functions are objects and can be passed as parameters .
+  //c is a function that takes a Conn as a parameter and returns a sequence of Nodes.
   def withConn(c:Conn => Seq[Node]):Seq[Node] = { //use [] for type parameters (generics).
     val connection =  datasource.getConnection
     val conn = new Conn(connection)
@@ -37,26 +39,30 @@ trait DBAdapter {
     def select(query:String, params:Param*)(r:ResultSet => Seq[Node]):Seq[Node] = {
       val result = prepareStatement(query, params:_*).executeQuery;
       //nested function.  Scalac will actually do tail call optimisation on this recursive method. means no stack overflow
-      def doRow(acc:Seq[Node], row:ResultSet):Seq[Node] = if (!row.next) acc else doRow(acc ++ r(row), row)
+      def doRow(acc:Seq[Node],row:ResultSet):Seq[Node] = if (!row.next) acc else doRow(acc ++ r(row), row)
       try {
 		  doRow(Seq(), result);
-      } finally { result.close}
+      } finally { result.close }
     }
   }
 }
-//Interact natively with java libraries. Here scala is being invoked by java as a servlet and scala is invoking java jdbc libraries
+//Interacts natively with java libraries. Here scala is being invoked by java
+//as a servlet and scala is invoking java jdbc libraries
 abstract class WebserviceServlet extends HttpServlet {
-  //XML literal syntax checked by compiler. If xml is not wellformed the class will not compile. code can be embedded using {}
-	def soapEnvelope(body:Node) = (<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+  //XML literal syntax checked by compiler. If xml is not wellformed the class will not compile.
+  //Code can be embedded using {}
+	def soapEnvelope(body:Node) = (<soapenv:Envelope
+                                xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
 		<soapenv:Header/>
 		<soapenv:Body>{body}</soapenv:Body>
     </soapenv:Envelope>)
 
    override def doPost(req:HttpServletRequest, resp:HttpServletResponse) = {
-    //Type inference - No need to declare types. Most of the time the compiler can infer what that type a value is by how it is used.
+    //Type inference - No need to declare types. Most of the time the compiler can infer what
+     //that type a value is by how it is used.
     val response = try {
 		val in = XML.load( req.getInputStream)
-		//Xpath like queries on XML. the \ operator is actually a method. in \ "Body' same as in.\("Body")
+		//Xpath like queries on XML. the \ operator is actually a method. in \ "Body" same as in.\("Body")
 		(in  \ "Body" \ "_").firstOption match {
 			case Some(payload) => handlePayload(payload)
 			case None => throw error("Payload element not found")
@@ -77,7 +83,8 @@ abstract class WebserviceServlet extends HttpServlet {
 class UserDetailsWSServlet extends WebserviceServlet with DBAdapter {
   //lazy value - evaluated once when it is first used
   lazy val datasource = (new javax.naming.InitialContext).lookup("java:/comp/env/jdbc/test").asInstanceOf[javax.sql.DataSource]
-  //Partially applied functions (currying) using multiple parameter set stntax. Calling mapUser(c) will return a function that takes a ResultSet and returns Seq[Node]
+  //Partially applied functions (currying) using multiple parameter set stntax.
+  //Calling mapUser(c) will return a function that takes a ResultSet and returns Seq[Node]
   def mapUser(c:Conn)(r:ResultSet):Seq[Node] =
     (<User id={"" + r.getLong("id")} firstName={r.getString("firstName")} lastName={r.getString("lastName")} >
      <Roles>
@@ -86,14 +93,14 @@ class UserDetailsWSServlet extends WebserviceServlet with DBAdapter {
 			})
       }</Roles>
      </User>)
-
   //pattern matching to extract data from objects
   def handlePayload(payload:Node):Node = payload match {
     case (<FindUsersRequest>{x}</FindUsersRequest>) => {
       val search = Param("%" + x + "%")
       (<Users xmlns="urn:www.scalaws.org:UserDetails:v1">
 			{withConn(c =>{
-			  c.select("select * from user_t where firstName like ? or lastName like ?", search,search)(mapUser(c))
+			  c.select("select * from user_t where firstName like ? or lastName like ?",
+					  search,search)(mapUser(c))
 			  })
 			}
       </Users>)
@@ -109,7 +116,8 @@ class UserDetailsWSServlet extends WebserviceServlet with DBAdapter {
                             Param( payload \ "@firstName"),
                             Param(payload \ "@lastName")                            )
 			c.insert("insert into user_t (id, firstName, lastName) values ( ?, ?, ?)", params:_*) //_* tells compiler that params is already a Seq (vararg)
-          (<Done/>)     //TODO:inserting roles left as exercise for reader
-		})(0) //Get first node
+          Seq()     //TODO:inserting roles left as exercise for reader
+		})
+		(<Done/>)
     }
 }
